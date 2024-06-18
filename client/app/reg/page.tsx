2edfc629de9ebc00243138c4c2cff6811e682302
Akzakoins <emirsasanov@gmail.com>
2024-06-19 00:20:10 +0300

    added reg page

--- client/app/reg/page.tsx
+++ client/app/reg/page.tsx
@@ -7,20 +7,27 @@ import { useDispatch } from "react-redux";
 import { setToken } from "../../store/authSlice";
 import { useRouter } from "next/navigation";
 
-const Login = () => {
+const Reg = () => {
   const dispatch = useDispatch();
-  const username = useInput("");
+
+  const name = useInput("");
+  const email = useInput("");
   const password = useInput("");
+  const phoneNumber = useInput("");
+
   const router = useRouter();
 
   const handleSubmit = () => {
     axios
-      .post("http://localhost:1000/auth/login", {
-        email: username.value,
+      .post("http://localhost:1000/auth/registration", {
+        name: name.value,
+        email: email.value,
         password: password.value,
+        phoneNumber: phoneNumber.value,
       })
       .then((res) => {
-        dispatch(setToken(res.data.token));
+        console.log(res);
+
         router.push("/");
         setTimeout(() => {
           window.location.reload();
@@ -44,12 +51,22 @@ const Login = () => {
     <Grid>
       <Paper elevation={10} style={paperStyle}>
         <TextField
-          label="Email"
-          placeholder="Enter email"
+          label="Username"
+          placeholder="Enter username"
           variant="outlined"
           fullWidth
           required
-          {...username}
+          {...name}
+          style={{ marginBottom: "10px" }}
+        />
+        <TextField
+          label="email"
+          placeholder="Enter email"
+          type="email"
+          variant="outlined"
+          fullWidth
+          required
+          {...email}
           style={{ marginBottom: "10px" }}
         />
         <TextField
@@ -63,6 +80,17 @@ const Login = () => {
           style={{ marginBottom: "10px" }}
         />
 
+        <TextField
+          label="Phone number"
+          placeholder="Enter phone number"
+          variant="outlined"
+          fullWidth
+          required
+          inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+          {...phoneNumber}
+          style={{ marginBottom: "10px" }}
+        />
+
         <Button
           type="submit"
           variant="contained"
@@ -70,16 +98,15 @@ const Login = () => {
           fullWidth
           onClick={handleSubmit}
         >
-          Sign in
+          register
         </Button>
 
         <Typography>
-          {" "}
-          Нет аккаунта ?<Link href="/reg">Зарегистрируйтесь</Link>
+          Уже есть аккаунт?<Link href="/login">Логин</Link>
         </Typography>
       </Paper>
     </Grid>
   );
 };
 
-export default Login;
+export default Reg;
